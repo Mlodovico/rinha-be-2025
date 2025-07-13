959d35e4034bf40633aed539b87c7d3a7c031125
Mlodovico <murilo.lodovico@outlook.com>
2025-07-13 17:21:20 -0300

    feature: condition to get payment json

--- src/modules/payment/controller/payment.controller.ts
+++ src/modules/payment/controller/payment.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import PaymentService from '../service/payment.service';
 import Payment from '../model/payment';
 
@@ -7,7 +7,12 @@ export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post('/api/payment')
-  createPayment(payment: Payment): Promise<void> {
+  createPayment(@Body() payment: Payment): Promise<void> {
+    // Validate the payment object here if necessary
+    if (!payment) {
+      throw new Error('Payment object cannot be empty');
+    }
+
     return this.paymentService.createPayment(payment);
   }
 }
